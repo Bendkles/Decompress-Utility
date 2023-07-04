7d3c8e2db3127a2c4ca4fba1feacdeee06097d9e
Shlomi <bendkles@duck.com>
2023-07-04 10:47:49 +0300

    new fixed versio

--- 2nd-task.sh
+++ 2nd-task.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# This script pings processes for a given executable and user
+# Usage: ./psping.sh [-c count] [-t timeout] [-u username] executable
+# -c: Number of times the script should ping (default: infinite)
+# -t: Time interval between pings in seconds (default: 1)
+# -u: Username to filter the processes (default: all users)
+# executable: Name of the executable to ping
+
 # Set default values
 count=-1
 timeout=1
@@ -9,13 +16,31 @@ user=""
 while getopts "c:t:u:" opt; do
   case $opt in
     c)
-      count=$OPTARG
+      # Validate that count is a positive integer
+      if [[ $OPTARG =~ ^[0-9]+$ ]]; then
+        count=$OPTARG
+      else
+        echo "Error: -c option requires a positive integer argument." >&2
+        exit 1
+      fi
       ;;
     t)
-      timeout=$OPTARG
+      # Validate that timeout is a positive integer
+      if [[ $OPTARG =~ ^[0-9]+$ ]]; then
+        timeout=$OPTARG
+      else
+        echo "Error: -t option requires a positive integer argument." >&2
+        exit 1
+      fi
       ;;
     u)
-      user=$OPTARG
+      # Validate that user exists
+      if id "$OPTARG" &>/dev/null; then
+        user=$OPTARG
+      else
+        echo "Error: User $OPTARG does not exist." >&2
+        exit 1
+      fi
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -34,7 +59,11 @@ exe_name=$1
 
 # Check if the executable name is provided
 if [ -z "$exe_name" ]; then
-  echo "Usage: psping [-c ###] [-t ###] [-u user-name] exe-name"
+  echo "Usage: psping [-c count] [-t timeout] [-u username] executable" >&2
+  echo "  -c: Number of times to ping (positive integer, default: infinite)" >&2
+  echo "  -t: Time interval between pings in seconds (positive integer, default: 1)" >&2
+  echo "  -u: Username to filter the processes (default: all users)" >&2
+  echo "  executable: Name of the executable to ping" >&2
   exit 1
 fi
 
@@ -44,8 +73,14 @@ ping_count=0
 # Loop indefinitely or until the ping count is reached
 while [ "$count" -lt 0 ] || [ $ping_count -lt "$count" ]; do
   # Count the number of live processes for the user and executable
-  live_count=$(pgrep -u "$user" "$exe_name" | wc -l)
+  live_count=$(pgrep -u "$user" "$exe_name" 2>/dev/null | wc -l)
 
+  # Check for errors in pgrep
+  if [ $? -ne 0 ]; then
+    echo "Error: Could not execute pgrep. Check permissions and executable name." >&2
+    exit 1
+  fi
+  
   # Echo the number of live processes
   echo "$live_count"
 
@@ -56,9 +91,3 @@ while [ "$count" -lt 0 ] || [ $ping_count -lt "$count" ]; do
   sleep "$timeout"
 done
 
-
-# ./psping [-c ###] [-t ###] [-u user-name] exe-name
-# To count the number of live processes for the user jack whose 
-#executable file is /opt/google/chrome/chrome --type=-broker every 2 seconds 
-# for a total of 5 pings, you can use the following command: bash
-# ./psping -c 5 -t 2 -u jack "/opt/google/chrome/chrome --type=-broker"
